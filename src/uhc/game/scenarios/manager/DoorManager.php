615cb2d4e7c60b869ef9e9205e3e708e146796fb
nathanpasdutout <nathanbaudrier@gmail.com>
2025-03-04 12:46:51 +0100

    add power and door systems for specific scenario

--- src/uhc/game/scenarios/manager/DoorManager.php
+++ src/uhc/game/scenarios/manager/DoorManager.php
@@ -1,1 +1,33 @@
 <?php
+
+namespace uhc\game\scenarios\manager;
+
+use pocketmine\block\Door;
+
+class DoorManager {
+
+    /**
+     * @var Door[]
+     */
+    private array $doors = [];
+
+    public function getAll(): array {
+        return $this->doors;
+    }
+
+    public function add(Door $door): void {
+        $this->doors[] = $door;
+    }
+
+    public function remove(Door $door): void {
+        unset($this->doors[array_search($door, $this->doors)]);
+    }
+
+    public function chooseRandom(): Door {
+        return $this->doors[array_rand($this->doors)];
+    }
+
+    public function exists(Door $door): bool {
+        return isset($this->doors[array_search($door, $this->doors)]);
+    }
+}
--- src/uhc/game/scenarios/manager/PowerManager.php
+++ src/uhc/game/scenarios/manager/PowerManager.php
@@ -1,1 +1,26 @@
 <?php
+
+namespace uhc\game\scenarios\manager;
+
+class PowerManager {
+
+    const int STRENGTH_POWER = 1;
+    const int RESISTANCE_POWER = 2;
+    const int JUMP_BOOST_POWER = 3;
+    const int SPEED_POWER = 4;
+    const int DOUBLE_LIFE_POWER = 5;
+
+    public function chooseRandom() : int {
+        return rand(self::STRENGTH_POWER, self::DOUBLE_LIFE_POWER);
+    }
+
+    public function getName(int $power) : string {
+        return match ($power) {
+            self::STRENGTH_POWER => "Force",
+            self::RESISTANCE_POWER => "Résistance",
+            self::JUMP_BOOST_POWER => "Saut Boosté",
+            self::SPEED_POWER => "Vitesse",
+            self::DOUBLE_LIFE_POWER => "Double Vie"
+        };
+    }
+}
