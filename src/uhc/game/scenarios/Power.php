1d60a50ad97c8a0041f6ac02c29d964f2877b073
nathanpasdutout <nathanbaudrier@gmail.com>
2025-05-19 11:55:21 +0200

    update Power system for SuperHero scenario

---
+++ src/uhc/game/scenarios/Power.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace uhc\game\scenarios;
+
+class Power {
+
+    const int STRENGTH_POWER = 0;
+    const int RESISTANCE_POWER = 1;
+    const int JUMP_BOOST_POWER = 2;
+    const int SPEED_POWER = 3;
+    const int DOUBLE_LIFE_POWER = 4;
+
+    private int $id;
+
+    public function __construct(int $id) {
+        $this->id = rand(self::STRENGTH_POWER, self::DOUBLE_LIFE_POWER);
+    }
+
+    public function getId() : int {
+        return $this->id;
+    }
+
+    public function getName(int $power) : string {
+        return match ($power) {
+            self::STRENGTH_POWER => "Force",
+            self::RESISTANCE_POWER => "Résistance",
+            self::JUMP_BOOST_POWER => "Saut Boosté",
+            self::SPEED_POWER => "Vitesse",
+            self::DOUBLE_LIFE_POWER => "Double Vie"
+        };
+    }
+}
