0bdb53b2b09b6f36682769d05a313633fa8050e8
Nathan <nathanbaudrier@gmail.com>
2025-07-14 14:23:15 +0200

    fix DisconnectTask when a player is afk for a long time (NOT TESTED)

--- src/uhc/tasks/DisconnectTask.php
+++ src/uhc/tasks/DisconnectTask.php
@@ -17,7 +17,7 @@ class DisconnectTask extends Task {
     }
 
     public function onRun() : void {
-        Main::getGame()->removePlayer($this->player);
+        Main::getInstance()->getGame()->removePlayer($this->player);
         Server::getInstance()->broadcastMessage(TextFormat::RED . $this->player->getName() . " s'est déconnecté depuis plus de 10 minutes, il est donc exlu de la partie.");
         $this->getHandler()->cancel();
     }
